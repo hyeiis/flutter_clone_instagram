a2fc2fd2f070a9e3b7feb8cdd96dd80f53712946
hyeis <hyeis0807@gmail.com>
2022-09-15 21:55:46 +0900

    commit

--- lib/src/pages/home.dart
+++ lib/src/pages/home.dart
@@ -1,10 +1,48 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_clone_instagram/src/components/image_data.dart';
 
 class Home extends StatelessWidget {
   const Home({super.key});
 
+  Widget _storyBoardList() {
+    return SingleChildScrollView(
+      scrollDirection: Axis.horizontal,
+      child: Row(
+        children: List.generate(
+          100,
+          (index) => Container(
+            decoration:
+                BoxDecoration(shape: BoxShape.circle, color: Colors.grey),
+          ),
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+        appBar: AppBar(
+          elevation: 0,
+          title: ImageData(IconsPath.logo, width: 270),
+          actions: [
+            GestureDetector(
+              onTap: () {},
+              child: Padding(
+                padding: const EdgeInsets.all(15.0),
+                child: ImageData(
+                  IconsPath.directMessage,
+                  width: 50,
+                ),
+              ),
+            )
+          ],
+        ),
+        body: ListView(
+          children: [
+            _storyBoardList(),
+            // _postList(),
+          ],
+        ));
   }
 }
